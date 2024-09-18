d450eba205ff355796eb5c03dad63b2c4f3dbde1
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-09-18 15:57:20 -0400

    APIkey

---
+++ js/calendar.js
@@ -0,0 +1,4 @@
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const API_KEY = 'AIzaSyCNuPc8INoIhejkZ0ZpBgqxMMubNbs7Twc';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
